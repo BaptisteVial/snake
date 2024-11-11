58727cd7dff7482a0be9ee1db89d7d95f1151687
Baptiste Vial <baptiste.vial@etu.minesparis.psl.eu>
2024-11-11 15:47:06 +0100

    fin du damier et initialisation du serpent

--- snake/proj/main.py
+++ snake/proj/main.py
@@ -1,60 +1,87 @@
+###  importing useful modules
+
 import argparse
 import pygame
+
+### defining default constants
+
 WIDTH_DEF = 400
 HEIGHT_DEF = 300
 COL_DEF = 20
 LIN_DEF = 20
 SQUARE_SIZE_DEF = 20
+SNAKE_POS_INIT = [(10, 5), (10, 6), (10, 7)]
 
+### for a better readability : defining the useful colors
 
-def hello():
-    parser = argparse.ArgumentParser(description='Some description.')
-    parser.add_argument('-a', help="A text explaining what is the use of the -a option and what type of value it takes.")
-    args = parser.parse_args()
+black = (0,0,0)
+white = (255,255,255)
+green = (0, 255, 0)
 
-def hello2():
-    print("Hello!")
+### defining the parser, including : number of colons, of lines, size of the tiles ; allowing to change default parameters
 
-def add(x, y):
-    return x + y
 
 def parse_args():
+
     parser = argparse.ArgumentParser(description='Some description.')
     # parser.add_argument('-W', '--width', type=int, default=WIDTH_DEF, help="Width of the screen")
     # parser.add_argument('-H', '--height', type=int, default=HEIGHT_DEF, help="Height of the screen")
     parser.add_argument('-C', '--colons', type=int, default=COL_DEF, help="Number of colons")
     parser.add_argument('-L', '--lines', type=int, default=LIN_DEF, help="Number of lines")
-    parser.add_argument('-S', '--square_size', type=int, default=SQUARE_SIZE_DEF, help="Size of checkers' squares")
+    parser.add_argument('-S', '--square_size', type=int, default=SQUARE_SIZE_DEF, help="Size of the tiles")
     args = parser.parse_args()
+    
     return args
 
-def checkerboards(screen, square_size, nb_cols, nb_lines):
-    color = (0, 0, 0) # black
-    for i in range(1, nb_lines):
-        for j in range(1, nb_cols):
-            rect = pygame.Rect(, top, square_size, square_size)
-            pygame.draw.rect(screen, color, rect)
+
+### drawing a checkerboard on a given screen
+
+
+def checkerboard(screen, square_size, nb_cols, nb_lines):
+    
+    for row in range(nb_lines):
+        for col in range(nb_cols):
+            if (row+col) % 2 == 0:      # alternance test
+                color = white
+            else :
+                color = black
+            pygame.draw.rect(screen, color, (col * square_size, row * square_size, square_size, square_size))
+
+
+### drawing the snake on a given checkerboard, where its position is given by a list of tiles (row,col)
+
+
+def drawing_snake(screen, snake_positions, square_size):
+    color = green
+    for pos in snake_positions:
+        row, col = pos
+        pygame.draw.rect(screen, color, (col * square_size, row * square_size, square_size, square_size))
+
+
+### defining the playable pygame program
+
 
 def snake():
+    ### initializing the arguments, the screen, the clock...
     args=parse_args()
     pygame.init()
     screen = pygame.display.set_mode( (args.square_size*args.colons, args.square_size*args.lines) )
     clock = pygame.time.Clock()
-    var_quit = True
+    running = True  # ending condition : running = False
 
-    while var_quit:
-
-    # Wait one second, starting from last display or now
-        clock.tick(100) # way more than one, to have a better speed execution
-    # Process new events (keyboard, mouse)
+    while running:
+        clock.tick(100) # >> 1 , to have a better speed execution
+    # Processing new events (keyboard, mouse)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                var_quit = False
+                running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
-                    var_quit = False  
+                    running = False  
 
-        screen.fill( (255, 255, 255) ) # Fill screen with white
-        checkerboards(screen, args.square_size, args.colons, args.lines)
+        screen.fill(white) # filling the screen with white
+        checkerboard(screen, args.square_size, args.colons, args.lines) # drawing a black/white checkerboard...
+        drawing_snake(screen, SNAKE_POS_INIT, args.square_size) #...with the snake on it
         pygame.display.update()
+    
     pygame.quit()
